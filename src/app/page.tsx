264635941f22bf2a17b418f56ef6d7e9cc3ac7fa
s-saito <s.s.saito108@gmail.com>
2023-08-02 01:36:49 +0900

    test tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import './globals.css'
 import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>home</h1>
-      <Link href="/page1">page1へ</Link>
+      <Link href="/page1">
+        <span className="bg-green-500 p-3 rounded text-xs text-white">page1へ</span>
+      </Link>
     </>
   );
 }
