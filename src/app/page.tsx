af502481192831f52c36b2c1c6bfe3261654eb03
Shinsuke Saito <s.s.saito108@gmail.com>
2023-10-25 21:57:34 +0900

    コンポーネントテスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,42 @@
-import './globals.css'
+"use client";
+import React, { useState } from "react";
+import "./globals.css";
 import Link from "next/link";
+import { Button } from "@ssaitho/react_ts_storybook/Button";
+import { DatePicker } from "@ssaitho/react_ts_storybook/DatePicker";
+import { StarIcon } from "@ssaitho/react_ts_storybook/Icon";
+import { MultiSelect } from "@ssaitho/react_ts_storybook";
+import { TextLink } from "@ssaitho/react_ts_storybook";
 
 export default function Home() {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
   return (
     <>
       <h1>home</h1>
+      <Button size="medium" variant="bg-blue-500">
+        react_ts_storybook
+      </Button>
+
+      <DatePicker
+        dateFormat="MMMM d, yyyy h:mm aa"
+        showTimeSelect
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        timeIntervals={1}
+      />
+      <MultiSelect />
+
+      <TextLink
+        href="/"
+        prefix={null}
+        suffix={<StarIcon height={16} width={16} />}
+        target="_blank"
+      >
+        TextLink
+      </TextLink>
+
       <Link href="/page1">
-        <span className="bg-green-500 p-3 rounded text-xs text-white">page1へ</span>
+        <button className="border px-3 py-2 text-xs">page1へ</button>
       </Link>
     </>
   );
