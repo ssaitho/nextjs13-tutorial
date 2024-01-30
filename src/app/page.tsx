6ed3afb82578e65b29fa74601e90eab3c8b1814f
Shinsuke Saito <s.s.saito108@gmail.com>
2024-01-30 18:00:35 +0900

    update react_ts_storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,25 @@ import React, { useState } from 'react'
 import './globals.css'
 import Link from 'next/link'
 import { Button } from '@ssaitho/react_ts_storybook/Button'
-// import { DatePicker } from "@ssaitho/react_ts_storybook/DatePicker";
-import { StarIcon } from "@ssaitho/react_ts_storybook/Icon";
-// import { MultiSelect } from "@ssaitho/react_ts_storybook";
 import { TextLink } from "@ssaitho/react_ts_storybook/TextLink";
+import { StarIcon } from "@ssaitho/react_ts_storybook/Icon";
+import { DatePicker } from "@ssaitho/react_ts_storybook/DatePicker";
+import { MultiSelect } from "@ssaitho/react_ts_storybook";
 
 export default function Home() {
   // const [startDate, setStartDate] = useState<Date | null>(new Date());
+
   return (
     <>
       <h1>home</h1>
-      <Button size="medium" variant="bg-blue-500">
+
+      <Button size="medium" variant="bg-red-500">
         react_ts_storybook
       </Button>
+
+      <Link href="/page1">
+        <button className="border px-3 py-2 text-xs">page1へ</button>
+      </Link>
 
       {/* <DatePicker
         dateFormat="MMMM d, yyyy h:mm aa"
@@ -24,20 +30,16 @@ export default function Home() {
         onChange={(date) => setStartDate(date)}
         timeIntervals={1}
       /> */}
+
       {/* <MultiSelect /> */}
 
       <TextLink
         href="/"
-        // prefix={null}
-        suffix={<StarIcon height={16} width={16} />}
+        prefix={<StarIcon height={16} width={16} />}
         target="_blank"
       >
         TextLink
       </TextLink>
-
-      <Link href="/page1">
-        <button className="border px-3 py-2 text-xs">page1へ</button>
-      </Link>
     </>
   )
 }
